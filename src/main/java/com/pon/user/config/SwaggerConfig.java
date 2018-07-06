2a3e91f2393db13a31667449c7fdf91b4133da32
Nathapon <pon_engine@hotmail.com>
2018-07-06 12:22:48 +0700

    test

--- src/main/java/com/pon/user/config/SwaggerConfig.java
+++ src/main/java/com/pon/user/config/SwaggerConfig.java
@@ -16,7 +16,7 @@ public class SwaggerConfig {
 	@Bean
     public Docket productApi() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .select()                 .apis(RequestHandlerSelectors.basePackage("com.pon.user.controller"))
+                .select().apis(RequestHandlerSelectors.basePackage("com.pon.user.controller"))
                 .paths(regex("/.*"))
                 .build();
     }
